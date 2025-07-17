80df2b4946d7e9f924075595932258b3416ec1ab
damienjoget <damien.ng@joget.com>
2025-07-17 12:41:54 +0800

    Fixed: MFA activation does not work properly when user is required to change password

--- src/main/java/org/joget/marketplace/MandatoryMfaSecureDirectoryManagerImpl.java
+++ src/main/java/org/joget/marketplace/MandatoryMfaSecureDirectoryManagerImpl.java
@@ -1,8 +1,11 @@
 package org.joget.marketplace;
 
 import org.joget.apps.app.service.AppUtil;
+import org.joget.commons.util.LogUtil;
+import org.joget.directory.model.service.DirectoryManagerPlugin;
 import org.joget.directory.model.service.UserSecurity;
 import org.joget.directory.model.service.UserSecurityFactory;
+import org.joget.plugin.base.PluginManager;
 import org.joget.plugin.directory.SecureDirectoryManagerImpl;
 import org.joget.plugin.directory.dao.UserExtraDao;
 import org.joget.plugin.directory.dao.UserPasswordHistoryDao;
@@ -14,8 +17,68 @@ public class MandatoryMfaSecureDirectoryManagerImpl extends SecureDirectoryManag
 
     private UserSecurity userSecurity;
 
+    public static final Integer NUM_OF_DM = 1;
+    private Map<String, Object> properties;
+    private Map<Integer, DirectoryManagerPlugin> dmps;
+    private Map<Integer, Map<String, Object>> dmpProperties;
+
     public MandatoryMfaSecureDirectoryManagerImpl(Map properties) {
         super(properties);
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return this.properties;
+    }
+
+    @Override
+    public void setProperties(Map<String, Object> properties) {
+        this.properties = properties;
+        this.dmps = new HashMap();
+        this.dmpProperties = new HashMap();
+        PluginManager pm = (PluginManager)AppUtil.getApplicationContext().getBean("pluginManager");
+
+        for(int i = 1; i <= NUM_OF_DM; ++i) {
+            try {
+                Object dmObject = this.getProperty("dm" + i);
+                if (dmObject != null && dmObject instanceof Map) {
+                    Map temp = (Map)dmObject;
+                    String className = temp.get("className").toString();
+                    if (!className.isEmpty()) {
+                        DirectoryManagerPlugin plugin = (DirectoryManagerPlugin)pm.getPlugin(className);
+                        if (plugin != null) {
+                            Map<String, Object> tempProperties = (Map)temp.get("properties");
+                            this.dmps.put(i, plugin);
+                            this.dmpProperties.put(i, tempProperties);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                LogUtil.error(SecureDirectoryManagerImpl.class.getName(), e, "");
+            }
+        }
+
+    }
+
+    @Override
+    public Object getProperty(String property) {
+        Object value = this.properties != null ? this.properties.get(property) : null;
+        return value;
+    }
+
+    @Override
+    public String getPropertyString(String property) {
+        String value = this.properties != null && this.properties.get(property) != null ? (String)this.properties.get(property) : "";
+        return value;
+    }
+
+    @Override
+    public void setProperty(String property, Object value) {
+        if (this.properties == null) {
+            this.properties = new HashMap();
+        }
+
+        this.properties.put(property, value);
     }
 
     /**
@@ -35,6 +98,10 @@ public class MandatoryMfaSecureDirectoryManagerImpl extends SecureDirectoryManag
         Map<String, Object> properties = getProperties();
         if (properties != null) {
             usProperties.putAll(properties);
+        }
+        if (dmps != null) {
+            usProperties.put("_dmps", dmps);
+            usProperties.put("_dmpProperties", dmpProperties);
         }
         userSecurity.setProperties(usProperties);
         return userSecurity;
--- src/main/java/org/joget/marketplace/MandatoryTotpMfaAuthenticator.java
+++ src/main/java/org/joget/marketplace/MandatoryTotpMfaAuthenticator.java
@@ -2,23 +2,37 @@ package org.joget.marketplace;
 
 import org.joget.apps.app.service.AppPluginUtil;
 import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.workflow.security.WorkflowUserDetails;
+import org.joget.commons.util.LogUtil;
+import org.joget.commons.util.ResourceBundleUtil;
 import org.joget.commons.util.SecurityUtil;
 import org.joget.directory.dao.UserMetaDataDao;
+import org.joget.directory.model.Role;
+import org.joget.directory.model.User;
 import org.joget.directory.model.UserMetaData;
 import org.joget.directory.model.service.DirectoryUtil;
+import org.joget.directory.model.service.ExtDirectoryManager;
 import org.joget.directory.model.service.UserSecurity;
 import org.joget.plugin.base.PluginManager;
 import org.joget.plugin.directory.TotpMfaAuthenticator;
+import org.joget.workflow.model.dao.WorkflowHelper;
+import org.joget.workflow.model.service.WorkflowUserManager;
 import org.joget.workflow.util.WorkflowUtil;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MandatoryTotpMfaAuthenticator extends TotpMfaAuthenticator {
 
@@ -66,7 +80,25 @@ public class MandatoryTotpMfaAuthenticator extends TotpMfaAuthenticator {
             } else if ("fetotp".equals(action)) {
                 content = wsForceEnableTotpAuthHandle(model, request, response);
             } else if ("fetotps".equals(action)) {
-                content = wsForceEnableTotpAuthSubmitHandle(model, request, response);
+                try {
+                    content = wsForceEnableTotpAuthSubmitHandle(model, request, response);
+                }catch (Exception e) {
+                    //if any loginPostProcessing after activating MFA throws exception (eg. password expiry)
+                    String systemAlert = AppUtil.getSystemAlert();
+                    if(systemAlert != null && systemAlert.length() > 0) {
+                        //logout
+                        WorkflowUserManager wum = (WorkflowUserManager) AppUtil.getApplicationContext().getBean("workflowUserManager");
+                        wum.setCurrentThreadUser(WorkflowUserManager.ROLE_ANONYMOUS);
+                        SecurityContextHolder.clearContext();
+
+                        HttpSession session = request.getSession(true);
+                        session.removeAttribute("SPRING_SECURITY_CONTEXT");
+
+                        //we are already inside a popup, inject script to make sure no nested PopupDialog, replace own (iframe) URL instead
+                        content = "<script>PopupDialog.prototype.show = function(){ window.location = this.src }</script>";
+                        content += systemAlert;
+                    }
+                }
             }
         }
 
@@ -136,8 +168,8 @@ public class MandatoryTotpMfaAuthenticator extends TotpMfaAuthenticator {
 
                     dao.addUserMetaData(data);
 
-                    return loginUser(username);
-                    //return "<script>parent.updateMFa(\"" + StringUtil.escapeString(SecurityUtil.encrypt(secret), "javascript", (Map)null) + "\");</script>";
+                    return loginUser(username, request);
+
                 } else {
                     model.put("secret", secret);
                     model.put("barcodeUrl", this.getQRBarcode(username, WorkflowUtil.getHttpServletRequest().getServerName(), secret));
@@ -171,5 +203,79 @@ public class MandatoryTotpMfaAuthenticator extends TotpMfaAuthenticator {
 
     protected String getMessage(String key) {
         return AppPluginUtil.getMessage(key, getClassName(), Activator.MESSAGE_PATH);
+    }
+
+    //temp fix for dx9
+    @Override
+    protected String wsVerifyPinSubmitHandle(Map model, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UserSecurity us = DirectoryUtil.getUserSecurity();
+
+        String tempusername = request.getParameter("u");
+        if (tempusername != null) {
+            UserMetaDataDao dao = (UserMetaDataDao) AppUtil.getApplicationContext().getBean("userMetaDataDao");
+
+            String username = SecurityUtil.decrypt(tempusername);
+            String nonce = request.getParameter("nonce");
+            UserMetaData usecret = dao.getUserMetaData(username, KEY);
+
+            if (usecret == null || !SecurityUtil.verifyNonce(nonce, new String[] {tempusername, usecret.getValue()})) {
+                return getTemplate("unauthorized", model);
+            }
+
+            try {
+                model.put("url", AppUtil.getRequestContextPath() + "/web/json/plugin/"+getClassName()+"/service?a=vps&u="+URLEncoder.encode(tempusername, "UTF-8")+"&nonce="+URLEncoder.encode(nonce, "UTF-8"));
+            } catch (UnsupportedEncodingException ex) {
+            }
+
+            String pin = request.getParameter("pin");
+            if (pin != null && pin.equals(getTOTPCode(SecurityUtil.decrypt(usecret.getValue())))) {
+                return loginUser(username, request);
+            } else {
+                model.put("error", ResourceBundleUtil.getMessage("totp.invalid"));
+            }
+        }
+
+        return getTemplate("verifyPin", model);
+    }
+
+    //temp fix for dx9
+    public String loginUser (String username, HttpServletRequest request) throws IOException {
+        ExtDirectoryManager dm = (ExtDirectoryManager) DirectoryUtil.getApplicationContext().getBean("directoryManager");
+        User user = dm.getUserByUsername(username);
+
+        Collection<Role> roles = dm.getUserRoles(username);
+        List<GrantedAuthority> gaList = new ArrayList<GrantedAuthority>();
+        if (roles != null && !roles.isEmpty()) {
+            for (Role role : roles) {
+                GrantedAuthority ga = new SimpleGrantedAuthority(role.getId());
+                gaList.add(ga);
+            }
+        }
+
+        // return result
+        UserDetails details = new WorkflowUserDetails(user);
+        UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(user.getUsername(), "", gaList);
+        result.setDetails(details);
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(result);
+
+        HttpSession session = request.getSession(true);
+        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+
+
+        HttpServletRequest httpRequest = WorkflowUtil.getHttpServletRequest();
+        String ip = AppUtil.getClientIp(httpRequest);
+        LogUtil.info(getClass().getName(), "Authentication for user " + username + " ("+ip+") : true");
+        WorkflowHelper workflowHelper = (WorkflowHelper) AppUtil.getApplicationContext().getBean("workflowHelper");
+        workflowHelper.addAuditTrail(this.getClass().getName(), "authenticate", "Authentication for user " + username + " ("+ip+") : true");
+
+        //post login processing
+        UserSecurity userSecurity = DirectoryUtil.getUserSecurity();
+        if (userSecurity != null) {
+            userSecurity.loginPostProcessing(user, null, true);
+        }
+
+        return "<script>parent.window.location = '"+getRedirectUrl()+"';</script>";
     }
 }
